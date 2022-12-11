2e7f0cf1c0f857f73ff0c1564416fe547629e495
DmytroMykolaichuk <110165245+DmytroMykolaichuk@users.noreply.github.com>
2022-12-11 06:55:12 +0000

    Deploying to gh-pages from @ DmytroMykolaichuk/mimino-adaptive-414px--1024px-1280px@3acf7ae6470ce86b923637da3072f6333441c675 🚀

---
+++ index.db5872d2.js
@@ -0,0 +1,2 @@
+$(".slider-for").slick({slidesToShow:1,slidesToScroll:1,arrows:!1,fade:!0,asNavFor:".slider-nav"}),$(".slider-nav").slick({slidesToShow:3,slidesToScroll:1,asNavFor:".slider-for",dots:!0,centerMode:!0,focusOnSelect:!0});
+//# sourceMappingURL=index.db5872d2.js.map
