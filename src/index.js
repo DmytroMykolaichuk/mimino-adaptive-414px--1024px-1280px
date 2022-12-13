ec475c9fe194973b1743a2b3eda7a5c1660d5bf0
Sergey Donyk <doncarlos@i.ua>
2022-12-13 02:59:27 +0200

    slick-gluk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav',
 });
 $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+});
+$('.two-time').slick({
+  slidesToShow: 1,
+  infinite: true,
+  speed: 300,
+  dots: false,
+  centerMode: true,
+  adaptiveHeight: true,
 });
