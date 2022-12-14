41ee2fb6d28c7ecd7fface96274d39201f20d8af
DmytroMykolaichuk <110165245+DmytroMykolaichuk@users.noreply.github.com>
2022-12-14 19:23:26 +0000

    Deploying to gh-pages from @ DmytroMykolaichuk/mimino-adaptive-414px--1024px-1280px@fb73e64f05e48a0d4f13d9ac2a6570decff0155b 🚀

--- triple-room.09dfb878.js
+++ triple-room.09dfb878.js
@@ -1,2 +1,2 @@
 $(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0}),$(".two-time").slick({dots:!1,infinite:!0,speed:300,slidesToShow:1,slidesToScroll:1,variableWidth:!0,autoplay:!0}),$(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:3,adaptiveHeight:!0}),$(".details-restaurant-slider").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,slidesToScroll:1,variableWidth:!0,autoplay:!0});
-//# sourceMappingURL=hotel.09ae6e87.js.map
+//# sourceMappingURL=triple-room.09dfb878.js.map
