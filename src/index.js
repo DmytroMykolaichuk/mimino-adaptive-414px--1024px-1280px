57f7193db73afa6a8bedf0eda3eb9dc6ed8f7ad9
Olexandr Horbenko <horbenko.o.a@gmail.com>
2022-12-15 16:26:24 +0200

    double-room-hero
    Add work branch

--- src/index.js
+++ src/index.js
@@ -33,3 +33,13 @@ $('.details-restaurant-slider').slick({
   variableWidth: true,
   autoplay: true,
 });
+
+$('.hero-slider').slick({
+  dots: false,
+  infinite: true,
+  // speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  variableWidth: false,
+  // autoplay: true,
+});
