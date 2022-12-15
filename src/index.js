29d5effd6142854819dee509667ddd944fb8d2ce
Anastasia Horbenko <anastasiakopylova00@gmail.com>
2022-12-15 14:25:42 +0200

    Tripple-room-hero
    Add slider

--- src/index.js
+++ src/index.js
@@ -43,3 +43,14 @@ $('.triple-room-slider').slick({
   variableWidth: true,
   autoplay: true,
 });
+
+
+$('.hero-slider').slick({ 
+  dots: false, 
+  infinite: true, 
+  // speed: 300, 
+  slidesToShow: 1, 
+  slidesToScroll: 1, 
+  variableWidth: false, 
+  // autoplay: true, 
+});
